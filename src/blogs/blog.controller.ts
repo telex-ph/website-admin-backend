809590aee09a20def6ef0b826dc71ba21befc774
domingohernani <dev.hernani.domingo@gmail.com>
2026-01-16 17:13:42 +0800

    feat: implement blog creation with validation using Zod

--- src/blogs/blog.controller.ts
+++ src/blogs/blog.controller.ts
@@ -1,17 +1,28 @@
 import type { Request, Response } from "express";
 import Blog from "./Blog.ts";
+import { createBlogSchema } from "./dto/create-blog.dto.ts";
 
 export const addBlog = async (req: Request, res: Response) => {
+  const parsed = createBlogSchema.safeParse(req.body);
+
+  if (!parsed.success) {
+    return res.status(400).json({
+      error: "Validation failed",
+      message: "Zod Error. Invalid request data",
+    });
+  }
+
+  const blog = parsed.data;
+
   try {
-    const article = await Blog.create({
-      title: "Awesome Post!",
-      slug: "awesome-post",
-      published: true,
-      content: "This is the best post ever",
-      tags: ["featured", "announcement"],
+    const neBlog = await Blog.create({
+      title: blog.title,
+      slug: blog.slug,
+      content: blog.content,
+      tags: blog.tags,
     });
 
-    res.status(200).json(article);
+    res.status(200).json(neBlog);
   } catch (error: unknown) {
     if (error instanceof Error) {
       console.error("Adding blog error:", error.message);
