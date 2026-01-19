3e6102f8765e5f5d301a036981c53a24614d00e2
domingohernani <dev.hernani.domingo@gmail.com>
2026-01-19 16:16:38 +0800

    feat: add JWT verification middleware to user registration route

--- src/users/user.router.ts
+++ src/users/user.router.ts
@@ -1,7 +1,8 @@
 import express from "express";
 import { addUser } from "./user.controller.ts";
+import { verifyJwt } from "../middlewares/verify-jwt.middleware.ts";
 const router = express.Router();
 
-router.post("/", addUser);
+router.post("/", verifyJwt, addUser);
 
 export default router;
