98a332ce43d7adf69c2ab5bf459ba457d6587a42
domingohernani <dev.hernani.domingo@gmail.com>
2026-01-20 13:20:24 +0800

    fix: make category field required in create blog schema

--- src/blogs/dto/create-blog.dto.ts
+++ src/blogs/dto/create-blog.dto.ts
@@ -6,7 +6,7 @@ export const createBlogSchema = z.object({
   // TODO: make the category enum instead and apply to other DTO
   status: z.enum(["published", "draft", "scheduled"]),
   // TODO: change the enum category, please notify hernani po.
-  category: z.enum(["categor1", "categor2", "categor3"]).optional(),
+  category: z.enum(["categor1", "categor2", "categor3"]),
   author: z.string().regex(/^[0-9a-fA-F]{24}$/, "Invalid ObjectId"),
 });
 
