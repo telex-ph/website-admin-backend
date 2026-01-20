7d2c8b091a3d91b48b8636cd0e7119c00ffa4925
domingohernani <dev.hernani.domingo@gmail.com>
2026-01-20 16:43:50 +0800

    refactor: enhance blog schema with required fields and remove unused DTO

--- src/blogs/Blog.ts
+++ src/blogs/Blog.ts
@@ -3,13 +3,25 @@ const { Schema, model } = mongoose;
 
 const blogSchema = new Schema(
   {
-    title: String,
-    slug: String,
-    status: String,
-    author: Schema.Types.ObjectId,
-    content: String,
-    category: String,
-    cover: String,
+    title: { type: String, required: true },
+    slug: { type: String, required: true },
+    status: {
+      type: String,
+      enum: ["published", "draft", "scheduled"],
+      required: true,
+    },
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    content: { type: String, required: true },
+    category: {
+      type: String,
+      enum: ["categor1", "categor2", "categor3"],
+      required: true,
+    },
+    cover: { type: String, required: true },
   },
   {
     timestamps: true,
