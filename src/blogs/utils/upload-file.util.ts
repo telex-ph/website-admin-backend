e3565bb58c6afa0a37264a6d65dbb963c6a4d026
domingohernani <dev.hernani.domingo@gmail.com>
2026-01-20 16:09:18 +0800

    feat: add cloudinary and multer configurations for file uploads

---
+++ src/blogs/utils/upload-file.util.ts
@@ -0,0 +1,12 @@
+import cloudinaryUpload from "../../common/configs/cloudinary.ts";
+
+const uploadFile = async (file: Express.Multer.File) => {
+  const result = await cloudinaryUpload(file.buffer, {
+    folder: `telex-admin/cover`,
+    resource_type: "auto",
+  });
+
+  return result.url;
+};
+
+export default uploadFile;
